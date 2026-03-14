db3e8650f8b66068093ba8e9edc04eee3a0209f2
Andrea Gil <agg300320@hotmail.com>
2026-03-14 12:20:16 -0700

    metadata

--- app/invitations/page.tsx
+++ app/invitations/page.tsx
@@ -3,14 +3,23 @@ import Invitation from "@/modules/invitation";
 export const metadata = {
   title: "Acompáñanos a celebrar la llegada de nuestro bebé",
   icons: {
-    icon: "/pato.png"
-  }
-}
+    icon: "/pato.png",
+  },
+  openGraph: {
+    title: "Invitación",
+    description: "¡Te invito a mi evento!",
+    type: "website",
+    images: [
+      {
+        url: "https://invitagil.com/preview.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
 function Invitations() {
-
-  return (
-    <Invitation />
-  );
+  return <Invitation />;
 }
 
 export default Invitations;
