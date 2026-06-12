627dc7cb2fad8c2f72c09ad4e62c473c3d2f2f9d
andreagg30 <agg300320@hotmail.com>
2026-06-11 22:26:31 -0700

    otp test

--- src/modules/main/index.tsx
+++ src/modules/main/index.tsx
@@ -1,6 +1,7 @@
 import useLogout from "../../api/useLogout";
 import { Button } from "../../components";
 import AppBar from "../../components/AppBar";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function Main() {
   const { mutate: logout, isPending } = useLogout();
@@ -11,6 +12,30 @@ function Main() {
       <p className="text-lg text-gray-600">
         Tu plataforma de gestión de eventos
       </p>
+      <Button
+        onClick={async () => {
+          const response = await fetch(`${API_URL}/test-mail`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          });
+
+          const data = await response.json();
+
+          console.log(data, 'datadata');
+          
+
+          if (!response.ok) {
+            throw new Error(data.message || "unknownError");
+          }
+
+          return data;
+        }}
+      >
+        test mail
+      </Button>
       <Button
         loading={isPending}
         onClick={() => {
